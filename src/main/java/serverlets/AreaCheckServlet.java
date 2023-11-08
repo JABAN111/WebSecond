8274f50fc320307371dafef03d4df7b6c3ba87ff
Jaba <368601@edu.itmo.ru>
2023-11-08 04:10:48 +0300

    added support of utf-8 for russian text, added processing for user data and added returned information for jsp page

--- src/main/java/serverlets/AreaCheckServlet.java
+++ src/main/java/serverlets/AreaCheckServlet.java
@@ -1,17 +1,55 @@
 package serverlets;
 
+import model.areaProcessing;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
+/**
+ * AreaCheckServlet, which checks whether a point hits an area on the coordinate plane and generates
+ * an HTML page with the results of the check. Must process all requests
+ * containing information about the coordinates of the point and the radius of the area.
+ */
+
+@WebServlet("/areaCheckServlet")
 public class AreaCheckServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setCharacterEncoding("UTF-8");
+
+        String timeZone = request.getParameter("localTime");
+        TimeZone userTimeZone = TimeZone.getTimeZone(timeZone);
+        TimeZone.setDefault(userTimeZone);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd\nHH:mm:ss");
+        Date currentTime = new Date();
+        String formattedTime = dateFormat.format(currentTime);
+
+        PrintWriter out = response.getWriter();
+
+            out.println("<tr>");
+            out.println("<td>" + request.getParameter("x") + "</td>");
+            out.println("<td>" + request.getParameter("y") + "</td>");
+            out.println("<td>" + request.getParameter("R") + "</td>");
+            out.println("<td>" + areaProcessing.areaCheck(
+                    Float.parseFloat(request.getParameter("x")),
+                    Double.parseDouble(request.getParameter("y")),
+                    Integer.parseInt(request.getParameter("R"))
+            ) + "</td>");
+
+            out.println("<td>" + formattedTime + "</td></tr>");
+            out.close();
+//        processRequest(request, response);
     }
 
 
@@ -19,9 +57,10 @@ public class AreaCheckServlet extends HttpServlet {
             throws ServletException, IOException
     {
         try{
-//            Float x = Float.parseFloat(request.getParameter("x"));
-//            Double y = Double.parseDouble(request.getParameter("y"));
-//            Integer R = Integer.parseInt(request.getParameter("R"));
+            Float x = Float.parseFloat(request.getParameter("x"));
+            Double y = Double.parseDouble(request.getParameter("y"));
+            Integer R = Integer.parseInt(request.getParameter("R"));
+            String statusOfHit= areaProcessing.areaCheck(x,y,R);
             PrintWriter out = response.getWriter();
             out.println("<body>ПРивет мираэ</body>");
         }catch (Exception e){
