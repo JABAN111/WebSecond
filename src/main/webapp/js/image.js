3a934feb178c4a1c781064ed60474f0924ebe1db
Jaba <368601@edu.itmo.ru>
2023-11-21 17:40:51 +0300

    seperated work of some functions

--- src/main/webapp/js/image.js
+++ src/main/webapp/js/image.js
@@ -7,12 +7,63 @@ let w = canvas.width,
 const hatchWidth = 20 / 2;
 const hatchGap = 56;
 
+let currentRadius = null;
+
+let sections = [
+    {
+        "name": "first_quarter",
+        "draw": () => {
+            ctx.beginPath();
+            ctx.fillStyle = "#0B04D5";
+            ctx.moveTo(w / 2, h / 2 - hatchGap * 2);
+            ctx.lineTo(w / 2 + hatchGap * 2, h / 2);
+            ctx.lineTo(w/2,h/2);
+            ctx.lineTo(w/2,h/2-hatchGap*2);
+            ctx.fill();
+            ctx.stroke();
+        }
+    },
+    {
+        "name": "second_quarter",
+        "draw": () => {
+            ctx.beginPath();
+            ctx.fillStyle = "#0B04D5";
+            ctx.moveTo(w / 2, h / 2);
+            ctx.fillRect(w / 2, h / 2, -hatchGap * 2, -hatchGap * 2);
+            // ctx.stroke(); // Убрал эту строку
+            ctx.closePath();
+        }
+    },
+    {
+        "name": "fourth_quarter",
+        "draw": () => {
+            ctx.beginPath();
+            ctx.fillStyle = "#0B04D5";
+            ctx.moveTo(w/2+hatchGap,h/2);
+            ctx.lineTo(w/2,h/2+hatchGap);
+            ctx.lineTo(w/2,h/2);
+            ctx.lineTo(w/2,h/2-hatchGap*2);
+            ctx.arc(w / 2, h / 2, hatchGap, 0, Math.PI / 2);
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+];
+
 function redrawGraph() {
     ctx.clearRect(0, 0, w, h);
-
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
-
+    for (const section of sections) {
+        section.draw();
+    }
+    drawAxesAndHatch();
+}
+function setRadius(newRadius) {
+    currentRadius = newRadius;
+    redrawGraph();
+}
+function drawAxesAndHatch() {
     // y ось
     ctx.beginPath();
     ctx.moveTo(w / 2, 0);
@@ -34,6 +85,7 @@ function redrawGraph() {
     ctx.lineTo(0, h / 2);
     ctx.stroke();
     ctx.closePath();
+
     //штрихи
     ctx.beginPath();
     ctx.moveTo(w / 2 - hatchWidth, h / 2 - hatchGap);
@@ -54,29 +106,64 @@ function redrawGraph() {
     ctx.lineTo(w / 2 + hatchGap * 2, h / 2 + hatchWidth);
     ctx.stroke();
     ctx.closePath();
-
-    ctx.beginPath();
-    ctx.fillStyle = "#0B04D5";
-    // 2 четверть
-    ctx.moveTo(w / 2, h / 2);
-    ctx.fillRect(w / 2, h / 2, -hatchGap * 2, -hatchGap * 2);
-    // 1 четверть
-    ctx.fillStyle = "#0B04D5";
-    ctx.moveTo(w / 2, h / 2 - hatchGap * 2);
-    ctx.lineTo(w / 2 + hatchGap * 2, h / 2)
-    //4 четверть
-    ctx.arc(w / 2, h / 2, hatchGap, 0, Math.PI / 2);
-    ctx.fill();
-    ctx.stroke();
+    numCoord();
 }
-redrawGraph()
-function printDotOnGraph(xCenter, yCenter, R, isHit) {
+/*Ставит числа рядом со штрихами в соответствии с выбранным R*/
+function numCoord(){
+    if(currentRadius === null)
+        return;
+    ctx.font = "20px Segue UI";
+    ctx.fillStyle = "black";
+    const indent = 20;
+    //ось x, положительная часть
+    ctx.fillText(`${currentRadius/2}`, w/2+hatchGap,h/2+indent);
+    ctx.fillText(`${currentRadius}`, w/2+hatchGap*2,h/2+indent);
+
+    //ось x, отрицательная часть
+    ctx.fillText(`${-currentRadius/2}`, w/2-hatchGap,h/2+indent);
+    ctx.fillText(`${-currentRadius}`, w/2-hatchGap*2,h/2+indent);
+
+    //ось y, положительная часть
+    ctx.fillText(`${currentRadius/2}`, w/2+indent,h/2-hatchGap);
+    ctx.fillText(`${currentRadius}`, w/2+indent,h/2-hatchGap*2);
+
+    //ось y, отрицательная часть
+    ctx.fillText(`${currentRadius/2}`, w/2+indent,h/2+hatchGap);
+    ctx.fillText(`${currentRadius}`, w/2+indent,h/2+hatchGap*2);
+}
+function printDotOnGraph(xCenter, yCenter, isHit) {
     redrawGraph();
     ctx.fillStyle = isHit ? '#1AFF00' : '#ff0000'
-    let x = w / 2 + xCenter * hatchGap * (2 / R);
-    let y = h / 2 - yCenter * hatchGap * (2 / R);
+    let x = w / 2 + xCenter * hatchGap * (2 / currentRadius);
+    let y = h / 2 - yCenter * hatchGap * (2 / currentRadius);
     ctx.beginPath();
     ctx.arc(x, y, 3, 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
 }
+
+let Rpicked = function(radius) {
+    // const canvas = document.getElementById('myCanvas');
+    // const ctx = ca   nvas.getContext('2d');
+
+    canvas.addEventListener('click', function(event) {
+        const rect = canvas.getBoundingClientRect();
+        console.log(rect.toJSON());
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        // ctx.beginPath();
+        // ctx.arc(200, 75, radius, 0, 2 * Math.PI);
+        // ctx.fill();
+        // ctx.closePath();
+
+        console.log(`X: ${x}, Y: ${y}, Radius: ${radius}\nIs Point In Path: ${ctx.isPointInPath(x, y)}`);
+
+        if (ctx.isPointInPath(x, y)) {
+            alert("You clicked inside the filled area");
+        }
+    });
+}
+
+
+redrawGraph();
